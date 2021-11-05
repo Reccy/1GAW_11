7d4e8896a7f2cafc20b2ba2c4e496275ba4c0c3f
Aaron Meaney <aaronmeaney@hotmail.com>
2021-11-05 16:11:28 +0000

    eh

--- Assets/Game/Core/EnemySpawner/EnemySpawner.cs
+++ Assets/Game/Core/EnemySpawner/EnemySpawner.cs
@@ -7,16 +7,12 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float m_ySpawnPosition;
     [SerializeField] private GameObject m_enemyPrefab;
     [SerializeField] private BoxCollider m_collider;
-    private Bounds m_spawnBounds;
+    private Vector3 SpawnBoundsMax => m_collider.transform.TransformPoint(m_collider.center + m_collider.size * 0.5f);
+    private Vector3 SpawnBoundsMin => m_collider.transform.TransformPoint(m_collider.center - m_collider.size * 0.5f);
 
     private float m_timeUntilNextSpawn = 5.0f;
     private float m_spawnPaddingMinSeconds = 5.0f;
     private float m_spawnPaddingMaxSeconds = 10.0f;
-
-    private void Awake()
-    {
-        m_spawnBounds = m_collider.bounds;
-    }
 
     private void FixedUpdate()
     {
@@ -31,11 +27,22 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnEnemy()
     {
-        float x = Random.Range(m_spawnBounds.min.x, m_spawnBounds.max.x);
-        float z = Random.Range(m_spawnBounds.min.z, m_spawnBounds.max.z);
+        float x = Random.Range(SpawnBoundsMin.x, SpawnBoundsMax.x);
+        float z = Random.Range(SpawnBoundsMin.z, SpawnBoundsMax.z);
 
         Vector3 spawnPosition = new Vector3(x, m_ySpawnPosition, z);
 
         Instantiate(m_enemyPrefab, spawnPosition, Quaternion.identity, null);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying)
+            return;
+
+        Debug.Log($"Min: {SpawnBoundsMin}, Max: {SpawnBoundsMax}");
+
+        Gizmos.DrawSphere(SpawnBoundsMin, 1.0f);
+        Gizmos.DrawSphere(SpawnBoundsMax, 1.0f);
     }
 }
